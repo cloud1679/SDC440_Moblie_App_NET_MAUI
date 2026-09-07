871066afeb323df42682ca47fd68e28294df9170
cloud1679 <cloud1679@icloud.com>
2026-09-07 19:17:39 -0400

    Basic_login_screen
    Initial set up without require requirements

---
+++ Basic_login_screen/Basic_login_screen/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Basic_login_screen;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Basic_login_screen/Basic_login_screen/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Basic_login_screen;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
